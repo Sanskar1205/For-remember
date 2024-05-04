abbeb0f9110b8bfab9a732dae3e4cedc78b73a38
Sanskar <sanskarmp1215@gmail.com>
2024-05-04 16:42:19 +0530

    Add my python file

---
+++ sanskar.py
@@ -0,0 +1,172 @@
+# a=input("Enter your name:")
+# print(a)
+# x=input("enter the first number:")
+# y=input("enter the second number:")
+# print(int(x)+int(y))
+# print(int(x)-int(y))
+# print(int(x)*int(y))
+# print(int(x)/int(y))
+# print(int(x)%int(y))
+
+# a="sanskar"
+# # print(a[0:7])or 
+# print(a[:7])
+# print(len(a))
+# print("sanskar is a", len(a),"letter word")
+# nm="harry"
+# print(nm[-4:-2])
+# print(len(nm))
+# str1="sanskar"
+# print(str1.isalnum())
+# print(str1.isalnum())
+# a="death note is good anime"
+# print(a.upper())
+# b="DJCJDJDKJ"
+# print(b.lower())
+# c="  vndnk vnvnvn  "
+# print(c.strip())
+# d="sanskar..!!"
+# print(d.rstrip(";!."))
+# e="sanskar"
+# print(e.replace("sansk","c"))
+# f="sanskar is good boy"
+# print(f.split(" "))
+# g="sanskar is good boy"
+# print(g.capitalize())
+# h="welcome to sanskar world"
+# print(h.capitalize())
+# i="Welcome to sanskar world"
+# print(i.center(30))
+# print(i.center(50,"-"))
+# j="sanskar"
+# countj=j.count("a")
+# print(countj)
+# k="sanskar !!!!"
+# print(k.endswith("!"))
+# str1="welcome to the console"
+# print(str1.endswith("o", 7, 10))
+# l="sanskar is very good boy"
+# print(l.find("good"))
+# print(l.find("kabir"))
+# m="sanskar is very good boy"
+# print(m.index("good"))
+# n="sanskar is very good boy"
+# print(n.isalnum())
+# nm="sanskarisverygoodboy"
+# print(nm.isalnum())
+# o="sanskarisverygoodboy"
+# print(o.isalpha())
+# oo="sanskarisverygoodboy12"
+# print(oo.isalpha())
+# p="sanskar is very good boy"
+# print(p.islower())
+# q="sanskar is very good boy"
+# print(q.isprintable())
+# str2="         "
+# print(str2.isspace()) 
+# r="sanskar world"
+# print(r.title())
+# rr="Sanskar World"
+# print(rr.title())
+# s="sanskar"
+# print(s.isupper())
+# print(s.islower())
+# t="python is programming language"
+# print(t.startswith("python"))
+# u="Sanskar"
+# print(u.swapcase())
+# v="python is a programming language"
+# print(v.title())
+
+# a=int(input("enter your age: "))
+# print("your age is: ", a)
+
+# # conditional operators
+# # < ,>,==,<=,>=,!= not equal to !=
+a = int(input("Enter the number: "))  # Convert input to integer
+if a > 18:
+    print("You can vote")
+else:
+    print("You can't vote")
+
+# appleprice=100
+# budget=int(input("Enter your budget:"))
+# if(budget-appleprice>100):
+#     print("sanskar you can buy apple")    
+# else:
+#     print("sanskar don't buy apple")
+# a=("Movie Data Calculator")
+# print(a.center(50, "-"))
+# moviesize=int(input("Enter the size of movie which you want to download:"))    
+# overalldata=int(input("Enter your overall data:"))
+# availabledata=int(input("Enter your available data:"))
+# minimumdata=int(input("how much data shouid be remain:"))
+# if(availabledata-moviesize>minimumdata):
+#     print("you can download")
+# elif(availabledata-moviesize==minimumdata):
+#     print("if you download your data will be over")
+# elif(availabledata-moviesize<minimumdata):
+#     print("you should not download")        
+# else:
+#     print("you should not download movie")
+
+# for k in range(45):
+#     print(k)
+#     if(k==46):
+#         print("solve the bug")
+#     else:
+#         print("let's goooo")
+
+
+
+# x=int(input("enter the value of x"))
+# match x:
+#     case 0:
+#         print ("x is zero")
+#     case 4:
+#         print("x is 4")    
+#     case _ if x!=90:
+#         print(x, "is not 90")
+#     case _ if x!=80:
+#         print(x,"is not 80")
+#     case _ :
+#         print(x)
+
+# name="sanskar"
+# for i in name:
+#     print(i)
+#     if(i=="n"):
+#         print("this is r")
+# colors=["red", "blue", "yellow"]   
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for k in range(3):
+#     print(k)                
+# for k in range(1, 20):
+#     print(k-1)
+# for k in range(1, 12, 9):
+#     print(k)
+
+
+# for i in range(10):
+#     if(i==7):
+#         break
+#     print(i)
+
+# for i in range(10):
+#     if(i==8):
+#         continue
+#     print("6 x",i, "=", i*6)
+
+
+
+
+
+
+
+
+
+
